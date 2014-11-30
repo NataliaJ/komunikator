3c139e2c86734522e75c756e9a4f3dbd93bb500b
NataliaJablonska <n.jablonska@outlook.com>
2014-11-30 11:51:19 +0100

    onPushReceive

--- app/src/main/java/pl/szkolenieandroid/testy/PushReceiver.java
+++ app/src/main/java/pl/szkolenieandroid/testy/PushReceiver.java
@@ -6,12 +6,18 @@ import android.util.Log;
 
 import com.parse.ParsePushBroadcastReceiver;
 
+import de.greenrobot.event.EventBus;
+
 /**
  * Created by Malgorzata Jablonska on 2014-11-30.
  */
-public class PushReciver extends ParsePushBroadcastReceiver {
+public class PushReceiver extends ParsePushBroadcastReceiver {
+
+    private EventBus bus = EventBus.getDefault();
+
     @Override
     protected void onPushReceive(Context context, Intent intent) {
         Log.e("###","onPushReceive");
+        bus.post(new UpdateMessagerListEvent());
     }
 }
---
+++ app/src/main/java/pl/szkolenieandroid/testy/UpdateMessagerListEvent.java
@@ -0,0 +1,7 @@
+package pl.szkolenieandroid.testy;
+
+/**
+ * Created by Malgorzata Jablonska on 2014-11-30.
+ */
+public class UpdateMessagerListEvent {
+}
