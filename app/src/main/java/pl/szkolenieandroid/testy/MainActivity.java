f6c5fe7709ae59b88eda5e547c38304011a4a05f
NataliaJablonska <n.jablonska@outlook.com>
2014-11-30 11:16:20 +0100

    ParsePush

--- app/src/main/java/pl/szkolenieandroid/testy/MainActivity.java
+++ app/src/main/java/pl/szkolenieandroid/testy/MainActivity.java
@@ -1,39 +1,36 @@
 package pl.szkolenieandroid.testy;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.parse.ParsePush;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    @InjectView(R.id.editText)
+    EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    @OnClick(R.id.send)
+    public void click() {
+        ParsePush push = new ParsePush();
+        push.setChannel("Giants");
+        push.setMessage(text.getText().toString());
+        push.sendInBackground();
     }
 }
